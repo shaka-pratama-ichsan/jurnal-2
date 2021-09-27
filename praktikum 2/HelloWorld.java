73435f123349c34c5a8201fd0a0a0f14443a7d98
shaka-pratama-ichsan <shakapratama2004@gmail.com>
2021-09-27 17:41:16 +0700

    HelloWorld.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args)	{
+			System.out.println ("Nama: Shaka");
+			System.out.println ("NIM: 6706213098");
+			System.out.println ("Kelas: 4503");
+	}
+}
